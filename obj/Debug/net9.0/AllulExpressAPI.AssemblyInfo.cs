4da52221ef967b6ad1d7228a283c6b9e17d524d1
mryamsh <mryamsh.mohamad@gmail.com>
2025-11-03 21:29:59 +0300

    dockerfileadded

--- obj/Debug/net9.0/AllulExpressAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/AllulExpressAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllulExpressAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f78e159472579c13c55b6874d57c12f25ff265e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0880c24c8899b13314028f7414de0ecd3de4fac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllulExpressAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllulExpressAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
