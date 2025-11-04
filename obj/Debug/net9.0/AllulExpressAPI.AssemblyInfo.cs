6e72fab59dd7d39b239be40eff1bce64ecce0f49
mryamsh <mryamsh.mohamad@gmail.com>
2025-11-04 22:58:09 +0300

    add bearerrr

--- obj/Debug/net9.0/AllulExpressAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/AllulExpressAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllulExpressAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccea0f918f575a66d6b370a2f69f44e5eb51ad81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f756097858eea45278bc5abc7c0215de7d6f9783")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllulExpressAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllulExpressAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
