ad65f3cc61e54bcaf19eee63f1ae2690b3e96a47
mryamsh <mryamsh.mohamad@gmail.com>
2026-01-03 21:11:45 +0300

    done 2

--- Models/Roles.cs
+++ Models/Roles.cs
@@ -5,7 +5,7 @@ public class Role
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public string? NormalizedName { get; set; }
+    public string? Normalized_Name { get; set; }
     public string? Description { get; set; }
 
     public ICollection<Employees>? Employees { get; set; }
