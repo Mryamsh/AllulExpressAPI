8799cd85f3bcdeeebcbfa2be064cbf79ec692d41
mryamsh <mryamsh.mohamad@gmail.com>
2026-01-10 21:01:07 +0300

    testing log

--- obj/Debug/net9.0/AllulExpressAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/AllulExpressAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllulExpressAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8d8c9bda64f7f8635619562db740f1810e8056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40e0b0729a3f7194301967198b1cd517fb6a9ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllulExpressAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllulExpressAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
